7435377f59e405b39e8980149eabca9ad72f7b02
Sandro <zony1234rbd@gmail.com>
2023-01-22 12:02:58 -0500

    Define POST /articles endpoint

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -8,7 +8,7 @@ export class ArticlesService {
 
   constructor(private prisma: PrismaService) {}
   create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+    return this.prisma.article.create({ data: createArticleDto });
   }
 
   findDrafts() {
